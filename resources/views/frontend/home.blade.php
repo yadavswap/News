999f8761860b2b495c4304d663a19c8fa5f6b1d1
Swapnil Yadav <swapnil2591997@gmail.com>
2020-12-26 17:32:10 +0530

    update lasted

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -94,13 +94,13 @@
               <div class="flex flex-wrap -m-4">
                 @foreach($latest->take(10) as $key => $l)
 
-                <div class="p-4 md:w-1/3">
+                <div class="p-4 md:w-1/6">
                   <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" href="{{url('article')}}/{{$l->pid}}" src="{{url('/posts')}}/{{$l->image}}" >
+                    <img class="lg:h-48 md:h-10 w-full object-cover object-center" href="{{url('article')}}/{{$l->pid}}" src="{{url('/posts')}}/{{$l->image}}" >
                     <div class="p-6">
                       <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{{$l->place}}</h2>
                       <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{$l->title}}</h1>
-                      <p class="leading-relaxed mb-3">{!! substr($l->description,0,300)!!}</p>
+                      <p class="leading-relaxed mb-3">{!! substr($l->description,0,100)!!}...</p>
                       <div class="flex items-center flex-wrap ">
                         <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"  href="{{url('article')}}/{{$l->pid}}">Learn More
                           <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
