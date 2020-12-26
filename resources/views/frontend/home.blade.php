a89b916b89bcef55bb933deabd2b2a7353ce2989
Swapnil Yadav <swapnil2591997@gmail.com>
2020-12-26 17:25:36 +0530

    update lasted

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -86,14 +86,17 @@
             </div>
         </div>
         @endforeach --}}
-    @foreach($latest->take(10) as $key => $l)
 
         <section class="text-gray-600 body-font">
+
             <div class="container px-5 py-24 mx-auto">
+
               <div class="flex flex-wrap -m-4">
+                @foreach($latest->take(10) as $key => $l)
+
                 <div class="p-4 md:w-1/3">
                   <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" href="{{url('article')}}/{{$l->pid}}" src="{{url('/posts')}}/{{$l->image}}" width="500px" height="500px">
+                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" href="{{url('article')}}/{{$l->pid}}" src="{{url('/posts')}}/{{$l->image}}" >
                     <div class="p-6">
                       <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{{$l->place}}</h2>
                       <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{$l->title}}</h1>
@@ -120,10 +123,13 @@
                     </div>
                   </div>
                 </div>
+                @endforeach
+
                      </div>
+
             </div>
+
           </section>
-          @endforeach
 
         
 
