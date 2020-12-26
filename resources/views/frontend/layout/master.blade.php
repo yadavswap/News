d0504e8298987a0fe7bda9e61d53e762ebee8af8
Swapnil Yadav <swapnil2591997@gmail.com>
2020-12-26 16:43:41 +0530

    update news

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <a class="weatherwidget-io" href="https://forecast7.com/en/21d1879d66/bhandara/" data-label_1="BHANDARA" data-label_2="WEATHER" data-theme="original" >BHANDARA WEATHER</a>
+    <br>
 <script>
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 </script>
