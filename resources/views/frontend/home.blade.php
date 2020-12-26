254779495ac4a28e1e6d4e0f0836a9dd52599c03
Swapnil Yadav <swapnil2591997@gmail.com>
2020-12-26 17:34:46 +0530

    update img

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -100,7 +100,7 @@
                     <div class="p-6">
                       <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{{$l->place}}</h2>
                       <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{$l->title}}</h1>
-                      <p class="leading-relaxed mb-3">{!! substr($l->description,0,100)!!}...</p>
+                      <p class="leading-relaxed mb-3">{!! substr($l->description,0,200)!!}...</p>
                       <div class="flex items-center flex-wrap ">
                         <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"  href="{{url('article')}}/{{$l->pid}}">Learn More
                           <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
